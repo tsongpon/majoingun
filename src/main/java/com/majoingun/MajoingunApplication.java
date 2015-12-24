fa8b0418873c6a2775d3e5ad4336a258c6d2b8be
dekaoun <ni@Supannikars-MacBook-Pro.local>
2015-12-24 14:21:59 +0700

    Init project

---
+++ src/main/java/com/majoingun/MajoingunApplication.java
@@ -0,0 +1,12 @@
+package com.majoingun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MajoingunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MajoingunApplication.class, args);
+    }
+}
---
+++ src/main/java/com/majoingun/controller/ExampleController.java
@@ -0,0 +1,14 @@
+package com.majoingun.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ExampleController {
+
+    @RequestMapping(value = "/api/majoingun/hello", method = RequestMethod.GET)
+    public String index(){
+        return "index";
+    }
+}
