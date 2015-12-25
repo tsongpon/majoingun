0a25949b89d4fea57b1d8f744e4ddf121d2c12e7
dekaoun <ni@Supannikars-MacBook-Pro.local>
2015-12-25 10:05:39 +0700

    Create register page

---
+++ src/main/java/com/majoingun/StaticResourceConfiguration.java
@@ -0,0 +1,16 @@
+package com.majoingun;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class StaticResourceConfiguration extends WebMvcConfigurerAdapter {
+    private static final String CLASSPATH_RESOURCE_LOCATIONS = "classpath:/static/";
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/majoingun/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS)
+                .setCachePeriod(3600);
+    }
+}
