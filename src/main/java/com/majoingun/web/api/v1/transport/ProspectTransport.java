b989217059350732f1f7ec457203a0a09897b199
tum <tum@abctech-thailand.com>
2016-01-04 10:59:38 +0700

    Fix bug

--- src/main/java/com/majoingun/web/api/v1/transport/ProspectTransport.java
+++ src/main/java/com/majoingun/web/api/v1/transport/ProspectTransport.java
@@ -2,36 +2,32 @@ package com.majoingun.web.api.v1.transport;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.Email;
-import org.hibernate.validator.constraints.NotBlank;
+import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.validation.constraints.Pattern;
 
 @Data
 public class ProspectTransport {
 
-    @NotBlank(message = "First name can not be blank")
+    @NotEmpty(message = "First name can not be blank")
     private String firstName;
 
-    @NotBlank(message = "Last name can not be blank")
+    @NotEmpty(message = "Las name can not be blank")
     private String lastName;
 
-    @NotBlank(message = "Please select gender")
     private String gender;
 
-    @NotBlank(message = "E-mail address us required")
-    @Email(message = "Email is not valid")
+    @Pattern(regexp="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\."
+            +"[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@"
+            +"(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?",
+            message = "Invalid email address format")
     private String email;
-
-    @NotBlank(message = "Contact number is required")
     @Pattern(regexp = "[\\s]*[0-9]*[1-9]+", message="Mobile phone must be a number")
     private String mobile;
 
-    @NotBlank(message = "University is required")
     private String university;
 
-    @NotBlank(message = "Please select interested fields")
     private String interestedFields[];
 
-    @NotBlank(message = "Please chose intern or permanent")
     private String type;
 }
